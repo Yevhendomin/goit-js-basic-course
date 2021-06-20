178f807274090235cef6a280e881c73d9819130b
Yevhen Domin <dominyevhen@gmail.com>
2021-06-20 18:56:25 +0300

    task-05 is added

--- goit-js-hw-01/js/task-02.js
+++ goit-js-hw-01/js/task-02.js
@@ -4,23 +4,34 @@ const total = 100;
 let ordered = 49;
 
 if (total - ordered >= ordered) {
+
     console.log('Заказ оформлен, с вами свяжется менеджер')
+
 } else {
+
     console.log('На складе недостаточно твоаров!')
+
 }
 
 ordered = 1;
 
 if (total - ordered >= ordered) {
+
     console.log('Заказ оформлен, с вами свяжется менеджер')
+
 } else {
+
     console.log('На складе недостаточно твоаров!')
+
 }
 
 ordered = 51;
 
 if (total - ordered >= ordered) {
+
     console.log('Заказ оформлен, с вами свяжется менеджер')
+
 } else {
+
     console.log('На складе недостаточно твоаров!')
 }
---
+++ goit-js-hw-01/js/task-05.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const CHINA = [100, 'Китай'];
+const CHILI = [250, 'Чили'];
+const AUSTRIA = [170, 'Австрия', 'Австрию'];
+const INDIA = [80, 'Индия', 'Индию'];
+const YAMAIKA = [120, 'Ямайка', 'Ямайку'];
+
+let inputCountry = prompt('В какую страну доставить товар?');
+inputCountry = inputCountry.toLowerCase();
+
+switch (inputCountry) {
+
+    case (CHINA[1].toLowerCase()):
+        console.log(`Доставка в ${CHINA[1]} будет стоить ${CHINA[0]} кредитов`)
+        break;
+
+    case (CHILI[1].toLowerCase()):
+        console.log(`Доставка в ${CHILI[1]} будет стоить ${CHILI[0]} кредитов`)
+        break;
+
+    case (AUSTRIA[1].toLowerCase()):
+        console.log(`Доставка в ${AUSTRIA[2]} будет стоить ${AUSTRIA[0]} кредитов`)
+        break;
+
+    case (YAMAIKA[1].toLowerCase()):
+        console.log(`Доставка в ${YAMAIKA[2]} будет стоить ${YAMAIKA[0]} кредитов`)
+        break;
+
+    default:
+        alert(`В вашей стране доставка не доступна`)
+}
