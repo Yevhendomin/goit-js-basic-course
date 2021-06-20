3fab42073abdcb010f53c4301b722e515755bdfb
Yevhen Domin <dominyevhen@gmail.com>
2021-06-20 20:26:51 +0300

    task-06 is added

---
+++ goit-js-hw-01/js/task-06.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let input = 0;
+let total = 0;
+
+input = prompt('Введите число и я его прибавлю к общей сумме');
+
+while (input != null) {
+
+    total = total + Number.parseInt(input);
+    input = prompt('Введите число и я его прибавлю к общей сумме');
+
+}
+
+alert(`Общая сумма чисел равна ${total}`);
