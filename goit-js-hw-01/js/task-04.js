6e991ff6a980df0fbb0ebdfb59f2f7429ca88658
Yevhen Domin <dominyevhen@gmail.com>
2021-06-20 16:44:34 +0300

    task-04 is added

---
+++ goit-js-hw-01/js/task-04.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const credits = 23580;
+const pricePerDroid = 3000;
+let totalPrice = 0;
+
+let countToBuy = prompt('Сколько дроидов вы хотите купить?');
+
+if (countToBuy === null) {
+
+    console.log('Отменено пользователем!')
+
+} else {
+
+    countToBuy = Number.parseInt(countToBuy);
+    totalPrice = countToBuy * pricePerDroid
+
+    if (totalPrice <= credits) {
+
+        console.log(`Вы купили ${countToBuy} дроидов, на счету осталось ${credits - totalPrice} кредитов.`)
+
+    } else {
+
+        console.log('Недостаточно средств на счету!')
+    }
+}
