7642f35795e057aff2fba73135ad90f1ee85d090
Yevhen Domin <dominyevhen@gmail.com>
2021-06-26 21:39:41 +0300

    Tasks 03 04 05 06 is added

---
+++ goit-js-hw-03/js/task-04.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const countTotalSalary = function(employees) {
+    let total = 0;
+    const salaries = Object.values(employees);
+    for (let salary of salaries) {
+        total += salary;
+    }
+    return total;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+
+console.log(countTotalSalary({})); // 0
+
+console.log(
+    countTotalSalary({
+        mango: 100,
+        poly: 150,
+        alfred: 80,
+    })
+); // 330
+
+console.log(
+    countTotalSalary({
+        kiwi: 200,
+        lux: 50,
+        chelsy: 150,
+    })
+); // 400
+
+/* 
+
+  ЗАДАНИЕ 4:
+  
+  Напиши функцию countTotalSalary(employees) принимающую объект зарплат. 
+  Функция считает общую сумму запрплаты работников и возращает ее. Каждое поле объекта, 
+  передаваемого в функцию, имеет вид "имя":"зарплата".
+
+   */
